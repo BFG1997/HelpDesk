f67fae378a7173de43b70e1b714f5983f8522f35
BFG1997 <babkinvvns@mail.ru>
2026-04-09 13:14:16 +0500

    Добавлены use cases

---
+++ src/backend/HelpDesk.Application/Features/Auth/Commands/LoginCommand.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HelpDesk.Application.Features.Auth.Commands
+{
+    internal class LoginCommand
+    {
+    }
+}
---
+++ src/backend/HelpDesk.Application/Features/Auth/DTOs/AuthResponse.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HelpDesk.Application.Features.Auth.DTOs
+{
+    internal class AuthResponse
+    {
+    }
+}
