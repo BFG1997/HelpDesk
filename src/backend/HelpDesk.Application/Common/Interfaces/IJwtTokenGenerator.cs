948726205c44d02efbd384ec4a5ce2d2b09a56c6
BFG1997 <babkinvvns@mail.ru>
2026-04-13 13:03:43 +0500

    Интерфейсы для хэширования пароля и jwt токена

---
+++ src/backend/HelpDesk.Application/Common/Interfaces/IJwtTokenGenerator.cs
@@ -0,0 +1,12 @@
+﻿using HelpDesk.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HelpDesk.Application.Common.Interfaces
+{
+    public interface IJwtTokenGenerator
+    {
+        string Generate(User user);
+    }
+}
