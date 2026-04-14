2421880a3f2c3b44d4899b81a7e9d2b0f63a634b
BFG1997 <babkinvvns@mail.ru>
2026-04-14 11:26:39 +0500

    Добавлены классы Infrastructure без реализации

---
+++ src/backend/HelpDesk.Infrastructure/Data/Configurations/NotificationConfiguration.cs
@@ -0,0 +1,5 @@
+namespace HelpDesk.Infrastructure.Data.Configurations;
+
+public class NotificationConfiguration
+{
+}
---
+++ src/backend/HelpDesk.Infrastructure/Services/JwtTokenGenerator.cs
@@ -0,0 +1,5 @@
+namespace HelpDesk.Infrastructure.Services;
+
+public class JwtTokenGenerator
+{
+}
