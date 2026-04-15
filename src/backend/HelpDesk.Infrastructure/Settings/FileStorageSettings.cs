1e454e93a3fd949cf11d77e2c5aaa3ec01c825ca
BFG1997 <babkinvvns@mail.ru>
2026-04-15 13:11:13 +0500

    Файлы settings для файлов и Jwt

---
+++ src/backend/HelpDesk.Infrastructure/Settings/FileStorageSettings.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HelpDesk.Infrastructure.Settings
+{
+    public class FileStorageSettings
+    {
+        public string UploadRootPath { get; set; } = string.Empty;
+    }
+}
